d23163321c092a2dbf56fa9aa12a01e729eded7e
Marcus <znepb@outlook.com>
2020-02-23 15:08:45 -0500

    Update /boot/files.cfg and add more installation info

---
+++ inst/installation.lua
@@ -0,0 +1,45 @@
+{
+    release = {20, 9},
+    releaseType = "beta",
+    spaceRequirement = 80000,
+    releaseDate = {
+        m = 2,
+        d = 23,
+        y = 2020
+    },
+    directories = {
+        "/bin/",
+        "/bin/ui",
+        "/boot/",
+        "/etc/",
+        "/home/",
+        "/lib/"
+    },
+    files = {
+        "/bin/ui/fancyshutdown.lua",
+        "/bin/ui/login.lua",
+        "/bin/ui/menu.lua",
+        "/bin/ui/run.lua",
+        "/bin/ui/shutdown.lua",
+        "/bin/ui/titlebar.lua",
+        "/bin/ui/tskmgr.lua",
+        "/bin/kernel.lua",
+        "/bin/shell.lua",
+        "/boot/files.cfg",
+        "/boot/package.path",
+        "/boot/sys.path",
+        "/etc/accounts.cfg",
+        "/etc/colors.cfg",
+        "/lib/util/file.lua",
+        "/lib/util/general.lua",
+        "/lib/util/http.lua",
+        "/lib/util/init.lua",
+        "/lib/util/json.lua",
+        "/lib/util/math.lua",
+        "/lib/json.lua",
+        "/lib/nfte.lua",
+        "/lib/sha256.lua",
+        "/lib/textbox.lua",
+        "/startup.lua"
+    }
+}
